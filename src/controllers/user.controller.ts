c795fc882648ac9d6cf4eecb68752da41b930b79
Ayanaw Mengesha <ayanawmengesha21@gmail.com>
2026-03-14 12:41:33 -0800

    feat: add get logged in user route

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -2,7 +2,7 @@ import type { Request, Response,  } from "express";
 import * as userServices from "../services/user.service.ts"
 import type { AuthRequest } from "../middlewares/auth.middleware.ts";
 import httpStatus from "http-status"
-export const getMe = async (req:AuthRequest,res:Response)=>{
+export const getMe = async (req:any,res:Response)=>{
     try {
         const user = await userServices.getCurrentUser(req.user?._id)
         return res.status(httpStatus.OK).json({
--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -1,9 +1,12 @@
 import {Router} from "express"
 import { login, logout, register } from "../controllers/auth.controller.ts"
+import { protect } from "../middlewares/auth.middleware.ts"
+import { getMe } from "../controllers/user.controller.ts"
 
 const router = Router()
 
 router.post("/register",register)
 router.post("/login",login)
 router.post("/logout",logout)
+router.get("/me", protect, getMe);
 export default router;
